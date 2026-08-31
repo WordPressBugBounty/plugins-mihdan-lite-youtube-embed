da643e91faedc8ef465e1db014652c0a2cda5499
Sean Murphy <sean@iamseanmurphy.com>
2026-08-31 20:04:07 +0000

    1.9.3

--- mihdan-lite-youtube-embed/vendor/composer/installed.php
+++ mihdan-lite-youtube-embed/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'mihdan/lite-youtube-embed',
-        'pretty_version' => '1.9.2',
-        'version' => '1.9.2.0',
-        'reference' => '2c4850fef08707158a8b70f4fc44d9418ab9168b',
+        'pretty_version' => '1.9.3',
+        'version' => '1.9.3.0',
+        'reference' => 'c4f7ca42f5743cb367d4686564b88973877afb7a',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'mihdan/lite-youtube-embed' => array(
-            'pretty_version' => '1.9.2',
-            'version' => '1.9.2.0',
-            'reference' => '2c4850fef08707158a8b70f4fc44d9418ab9168b',
+            'pretty_version' => '1.9.3',
+            'version' => '1.9.3.0',
+            'reference' => 'c4f7ca42f5743cb367d4686564b88973877afb7a',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
